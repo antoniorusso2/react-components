ec7b4f2a09ef99d6c5d930be14a61ea69da45506
antoniorusso2 <russoantonio303@outlook.it>
2024-11-27 17:05:00 +0100

    - completed footer and main proportions, responsive font size with media queries

--- src/components/footer-section/footer.jsx
+++ src/components/footer-section/footer.jsx
@@ -3,8 +3,10 @@ import Style from './footer.module.css';
 function Footer() {
   return (
     <footer className={Style.footer}>
-      <div className="container">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia magni asperiores pariatur? Corporis rem debitis, qui cupiditate saepe, repellat inventore officiis nemo quibusdam cumque vel maxime sapiente voluptatem modi voluptatum!
+      <div className='container'>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia magni asperiores pariatur? Corporis rem debitis, qui cupiditate saepe, repellat inventore officiis nemo quibusdam cumque vel maxime sapiente voluptatem modi voluptatum!
+        </p>
       </div>
     </footer>
   );
